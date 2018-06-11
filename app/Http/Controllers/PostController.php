0584afafdc44d1f1fedce113c50fda38ffedbe7d
Ilija Savic <savic.ilija85@gmail.com>
2018-06-11 15:09:06 +0200

    Security with WYSIWYG Editor

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -8,6 +8,7 @@ use App\Post;
 use App\Tag;
 use App\Category;
 use Session;
+use Purifier;
 
 class PostController extends Controller
 {
@@ -61,7 +62,7 @@ class PostController extends Controller
         $post->title = $request->title;
         $post->slug = $request->slug;
         $post->category_id = $request->category_id;
-        $post->body =  $request->body;
+        $post->body = Purifier::clean($request->body);
 
         $post->save();
 
@@ -140,7 +141,7 @@ class PostController extends Controller
         $post->title = $request->input('title');
         $post->slug = $request->input('slug');
         $post->category_id = $request->input('category_id');
-        $post->body = $request->input('body');
+        $post->body = Purifier::clean($request->input('body'));
 
         $post->save();
 
